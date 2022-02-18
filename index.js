7c791632ea123dc2c24b455fe15f2aceccd92d60
Borys Medvediev <borys.medvediev@auto1.com>
2022-02-19 01:03:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const API_URL = 'https://api.adviceslip.com/advice';
+
+const elements = {
+    title: document.querySelector('[data-advice-title]'),
+    text: document.querySelector('[data-advice-text]'),
+    button: document.querySelector('[data-advice-button]'),
+};
+
+const getAdvice = async () => {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    return data?.slip;
+};
+
+const updateView = ({ id, advice }) => {
+    elements.title.textContent = `advice #${id}`;
+    elements.text.textContent = `"${advice}"`;
+};
+
+async function renderAdvice() {
+    let data = await getAdvice();
+
+    updateView(data);
+}
+
+elements.button.addEventListener('click', renderAdvice);
+
+renderAdvice();
